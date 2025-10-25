46cc5613ed16cfe7461117a3c4da9d78492c7aa8
Liu Dian <2813803402@qq.com>
2025-10-25 22:35:47 +0800

    use @RequestMapping("/depts")

--- tlias-web-management/src/main/java/com/loedean/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/loedean/controller/DeptController.java
@@ -9,20 +9,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/depts")
 @RestController
 public class DeptController {
 
     @Autowired
     private DeptService deptService;
 //    @RequestMapping(value = "/depts", method = RequestMethod.GET)
-    @GetMapping("/depts")
+    @GetMapping
     public Result list(){
         System.out.println("查询全部的部门数据");
         List<Dept> deptList = deptService.findAll();
         return Result.success(deptList);
     }
 
-    @GetMapping("/depts/{id}")
+    @GetMapping("/{id}")
     public Result get(@PathVariable("id") Integer id){
         System.out.println("查询部门id:" + id);
         Dept dept = deptService.getById(id);
@@ -46,21 +47,21 @@ public class DeptController {
 //        return Result.success();
 //    }
 
-    @DeleteMapping("/depts")
+    @DeleteMapping
     public Result delete(Integer id){
         System.out.println("删除部门:" + id);
         deptService.deleteById(id);
         return Result.success();
     }
 
-    @PostMapping("/depts")
+    @PostMapping
     public Result add(@RequestBody Dept dept){
         System.out.println("添加部门:" + dept);
         deptService.save(dept);
         return Result.success();
     }
 
-    @PutMapping("/depts")
+    @PutMapping
     public Result update(@RequestBody Dept dept){
         System.out.println("修改部门:" + dept);
         deptService.update(dept);
