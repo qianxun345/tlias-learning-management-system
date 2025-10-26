0d13f4ff44225ed68648a25c625e68da4b2eae31
Liu Dian <2813803402@qq.com>
2025-10-26 21:23:49 +0800

    init 员工信息类

---
+++ tlias-web-management/src/main/java/com/loedean/service/impl/EmpServiceImpl.java
@@ -0,0 +1,8 @@
+package com.loedean.service.impl;
+
+import com.loedean.service.EmpService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmpServiceImpl implements EmpService {
+}
