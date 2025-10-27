8d0482c4ac2ce769c1cd37a21365301e0d69e3ce
Liu Dian <2813803402@qq.com>
2025-10-27 23:03:19 +0800

    add PageHelper plugin

--- tlias-web-management/src/main/java/com/loedean/mapper/EmpMapper.java
+++ tlias-web-management/src/main/java/com/loedean/mapper/EmpMapper.java
@@ -8,10 +8,9 @@ import java.util.List;
 
 @Mapper
 public interface EmpMapper {
-    @Select("select count(*) from emp")
-    Long count();
 
     @Select("select emp.*, dept.name deptName from emp left join dept on emp.dept_id = dept.id " +
-            "order by emp.update_time desc limit #{start}, #{pageSize}")
-    List<Emp> list(Integer start, Integer pageSize);
+            "order by emp.update_time desc")
+    List<Emp> list();
+
 }
--- tlias-web-management/src/main/java/com/loedean/service/impl/EmpServiceImpl.java
+++ tlias-web-management/src/main/java/com/loedean/service/impl/EmpServiceImpl.java
@@ -1,5 +1,7 @@
 package com.loedean.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.loedean.mapper.EmpMapper;
 import com.loedean.pojo.Emp;
 import com.loedean.pojo.PageResult;
@@ -14,13 +16,33 @@ public class EmpServiceImpl implements EmpService {
 
     @Autowired
     private EmpMapper empMapper;
+//    @Override
+//    public PageResult<Emp> page(Integer page, Integer pageSize) {
+//        Long total = empMapper.count();
+//        Integer start = (page - 1) * pageSize;
+//        List<Emp> rows = empMapper.list(start, pageSize);
+//
+//        return new PageResult<>(total, rows);
+//
+//    }
+
+     /*
+     使用PageHelper
+     注意事项：
+     1. sql语句结尾不能加分号
+     2. 紧跟着PageHelper.startPage()的第一条sql语句才生效
+     */
     @Override
     public PageResult<Emp> page(Integer page, Integer pageSize) {
-        Long total = empMapper.count();
-        Integer start = (page - 1) * pageSize;
-        List<Emp> rows = empMapper.list(start, pageSize);
-
-        return new PageResult<>(total, rows);
-
+//        PageHelper.startPage(page, pageSize);
+//        List<Emp> empList = empMapper.list();
+//        Page<Emp> p = (Page<Emp>) empList;
+//        return new PageResult<>(p.getTotal(), p.getResult());
+        // 推荐写法
+        try (Page<Emp> p = PageHelper.startPage(page, pageSize)) {
+            List<Emp> empList = empMapper.list();
+            return new PageResult<>(p.getTotal(), p.getResult());
+        }
+        // 自动调用 p.close() 清理资源
     }
 }
